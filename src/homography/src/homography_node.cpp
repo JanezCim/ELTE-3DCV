51aef9e32acd5f2b6ad26dbca6e60a36ff64a433
Janez Cimerman <j.cimko@gmail.com>
2019-12-24 17:57:42 +0100

    Added base homography package

---
+++ src/homography/src/homography_node.cpp
@@ -0,0 +1,19 @@
+#include "ros/ros.h"
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+	if (argc != 2){
+		cout << "No arguments found" << endl;
+		return -1;
+	}
+  
+  return 0;
+}
